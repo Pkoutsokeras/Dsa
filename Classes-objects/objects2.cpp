f95e08e856dacffd7e9292487b9b2f5e02f01c1c
Petros Koutsokeras <46537214+Pkoutsokeras@users.noreply.github.com>
2020-01-02 18:43:12 +0200

    Added objects2 and moved objects1 to forlder

---
+++ Classes-objects/objects2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Box{
+    public:
+    //Constructor decleration with default values
+    Box(double l=1, double w=1, double h=1);
+    //member function
+    double volume();
+    private:
+    //member data
+        double  length;
+        double width;
+        double height;
+};
+//constructor using intializer list 
+Box::Box(double l,double w, double h): length(l),width(w),height(h){}
+double Box::volume(){return length*width*height;}
+int main(){
+    Box b1(10,5,10);
+    cout << "The volume is " << b1.volume()<< endl;
+    Box*pb = new Box();
+    cout << "The volume is " << pb -> volume() << endl;
+    delete pb;
+    return 0;
+}
